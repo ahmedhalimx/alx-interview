718d8cea467bb0d9449ba11a029833611dd515b9
ahmedhalimx <ahmedhalimcs101@gmail.com>
2024-11-21 23:03:55 +0200

    solved

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Matrix Transpose
+"""
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List]):
+    """
+    Compute The Transpose of a Matrix
+    Args:
+        matrix (list[[list]]): a matrix
+    """
+    n = len(matrix)
+    for i in range(int(n / 2)):
+        y = (n - i - 1)
+        for j in range(i, y):
+            x = (n - 1 - j)
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[x][i]
+            matrix[x][i] = matrix[y][x]
+            matrix[y][x] = matrix[j][y]
+            matrix[j][y] = tmp
